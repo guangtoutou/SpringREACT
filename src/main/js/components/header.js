6e8a81d769881c5df56a17aa72bdb9aca15faf2c
guangtoutou <ni.ningning@gmail.com>
2017-06-04 23:51:12 +0800

    modify header

--- src/main/js/components/header.js
+++ src/main/js/components/header.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 
 export default class Header extends React.Component{
+  constructor(){
+    super();
+    this.state = {title:'James'};
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e){
+    this.setState({title:e.target.value});
+  }
+
   render(props){
     return(
       <div>
-        <h1>hello, {this.props.title}</h1>
-        <button onClick={this.props.onClick}>click</button>
+        <h1>hello, {this.state.title}</h1>
+        <input onChange={this.handleChange}/>
       </div>
     );
   }
